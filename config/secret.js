d72ce44c1c8648c829a7d83d1e7427f6f2e71242
Vedret Hrvanovic <vedret.hrvanovic@gmail.com>
2018-08-08 10:51:13 +0200

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,4 +1,4 @@
 module.exports={
-    database:'mongodb://spin:SulciC21@ds141611.mlab.com:41611/wedrettweeter',
+    database:'mongodb://user:user123@ds141611.mlab.com:41611/wedrettweeter',
     secret:'abcd123'
 }
