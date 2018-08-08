8d95ad851d2bcc81901360dd709141bb603095f1
vedret.hrvanovic@gmail.com <wedretwedret>
2018-08-08 13:44:45 +0200

    Saving data to DB

--- realtime/io.js
+++ realtime/io.js
@@ -1,3 +1,7 @@
+const async = require('async');
+const Tweet = require('../models/tweet');
+const User = require('../models/user');
+
 module.exports=function(io){
     io.on('connection',function(socket){
 
@@ -6,7 +10,42 @@ module.exports=function(io){
         console.log(user.name);
     
     socket.on('tweet',(data)=>{
-        io.emit('incommingTweet',{data,user});
+        
+
+        //emit
+        //save data to the database
+        //push tweets 
+
+        async.parallel([
+           function(callback){
+            io.emit('incommingTweet',{data,user});
+           },
+
+           function(callback){
+               async.waterfall([
+                   function(callback){
+                       let tweet= new Tweet();
+                       tweet.content=data.content;
+                       tweet.owner=user._id;
+                       tweet.save(function(err){
+                           callback(err,tweet);
+                       })
+                   },
+                   function(tweet ,callback){
+
+                    User.update({
+                        _id:user._id
+                    },
+                    {
+                        $push:{tweets:{tweet:tweet._id}},
+                    },function(err,count){
+                        callback(err,count);
+                    }
+                );
+                 }              
+               ]);
+           }
+        ]);
     });
 });
 }
